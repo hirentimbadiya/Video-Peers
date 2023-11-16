80720bb9ea9e5bcaf2dfe7b908eda3d8da025abe
heyhiru <hirentimbadiya74@gmail.com>
2023-11-17 00:10:29 +0530

    added remote url
    Signed-off-by: heyhiru <hirentimbadiya74@gmail.com>

--- client/src/context/SocketProvider.jsx
+++ client/src/context/SocketProvider.jsx
@@ -9,7 +9,7 @@ export const useSocket = () => {
 }
 
 const SocketProvider = (props) => {
-    const socket = useMemo(() => io("localhost:8080"), []);
+    const socket = useMemo(() => io("https://video-peers-server.onrender.com/"), []);
     return (
         <SocketContext.Provider value={socket}>
             {props.children}
