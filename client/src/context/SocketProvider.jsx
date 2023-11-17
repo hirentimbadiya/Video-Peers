605ff80aa0ba28678e2adb98d766ec1c36efdf17
heyhiru <hirentimbadiya74@gmail.com>
2023-11-17 10:15:22 +0530

    added remote url in frontend
    Signed-off-by: heyhiru <hirentimbadiya74@gmail.com>

--- client/src/context/SocketProvider.jsx
+++ client/src/context/SocketProvider.jsx
@@ -9,7 +9,7 @@ export const useSocket = () => {
 }
 
 const SocketProvider = (props) => {
-    const socket = useMemo(() => io("localhost:8080"), []);
+    const socket = useMemo(() => io("video-peers-server.onrender.com/"), []);
     return (
         <SocketContext.Provider value={socket}>
             {props.children}
